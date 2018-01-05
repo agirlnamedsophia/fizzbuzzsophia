a9c123d2c8d80496db091336bef2613318e34eb3
agirlnamedsophia <sophia.russell@betterment.com>
2018-01-04 22:22:25 -0500

    Paginate posts in the api

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::PostsController < ApplicationController
-  def show
-    @post = Post.find params[:id]
-    render json: @post
+  def index
+    @posts = Post.ordered.page(params[:page]).per(2)
+    render json: @posts
   end
 end
