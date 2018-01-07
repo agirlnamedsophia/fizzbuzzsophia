14f19097d667413b374f4fa7dfbdc2268b325da9
agirlnamedsophia <sophia.russell@betterment.com>
2018-01-07 15:11:04 -0500

    remove auth

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,22 +1,6 @@
 class Api::V1::PostsController < ApiController
-
   def index
-    @posts = Post.ordered.page(params[:page]).per(2)
-    render json: @posts, current_page: @posts.current_page, next_page: @posts.next_page
-  end
-
-  def create
-    post = Post.new(create_params)
-    if post.valid?
-      post.save!
-    else
-      Rails.logger.info "post invalid #{create_params}"
-    end
-  end
-
-  private
-
-  def create_params
-    params.require(:post).permit(:title, :body)
+    @posts = Kaminari.paginate_array(Post.ordered).page(params[:page]).per(1)
+    render json: @posts.map(&:to_json), current_page: @posts.current_page, next_page: @posts.next_page
   end
 end
